010c97c0c22a98e4a192088d9289f68fcca87d7a
jef08 <jasfielding@gmail.com>
2025-04-09 11:09:39 +0200

    added two more movies

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
+//botones//
 const fellowship = document.querySelector(".fellowship");
 const peliculas = document.querySelector(".peliculas");
 const volver = document.querySelector(".volver");
-const fellowshipInfo = document.querySelector(".fellowship-info")
+const towers = document.querySelector(".towers");
+const king = document.querySelector(".king");
 
+//Info divs//
+const fellowshipInfo = document.querySelector(".fellowship-info");
+const towersInfo = document.querySelector(".towers-info");
+const kingInfo = document.querySelector(".king-info");
+
+//No display on load/"home"//
 volver.style.display = "none";
 fellowshipInfo.style.display = "none";
+towersInfo.style.display = "none";
+kingInfo.style.display = "none";
 
+//Button event listeners//
 volver.addEventListener("click", () => {
     peliculas.style.display = "flex";
     volver.style.display = "none";
     fellowshipInfo.style.display = "none";
+    towersInfo.style.display = "none";
+    kingInfo.style.display = "none";
 })
 
 fellowship.addEventListener("click", () => {
     peliculas.style.display = "none";
-    document.querySelector(".fellowship-info").style.display = "flex";
-    document.querySelector(".volver").style.display = "block";
+    fellowshipInfo.style.display = "flex";
+    volver.style.display = "block";
+})
+
+towers.addEventListener("click", () => {
+    peliculas.style.display = "none";
+    towersInfo.style.display = "flex";
+    volver.style.display = "block";
+})
+
+king.addEventListener("click", () => {
+    peliculas.style.display = "none";
+    kingInfo.style.display = "flex";
+    volver.style.display = "block";
 })
